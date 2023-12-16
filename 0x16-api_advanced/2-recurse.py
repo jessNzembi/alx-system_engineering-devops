36007f3f0546cdf50258c14d5e7e59358571653d
Jess <jessgatura@gmail.com>
2023-12-16 15:21:40 +0300

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ hot articles for a given subreddit recrusively
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], count=0, after=None):
     """
     Returns a list containing the titles of all hot articles
     """
