6bf43c6477ca2845cce8140f4001c4e8df512931
Jess <jessgatura@gmail.com>
2023-12-16 15:14:22 +0300

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,13 @@ def top_ten(subreddit):
     """Prints the top 10 hot posts of a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers).json()
+    param = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=param,
+                            allow_redirects=False).json
 
-    if response.get("error") == 404:
+    if response.status_code == 404:
         print("None")
         return 0
     for post in response.get("data").get("children"):
